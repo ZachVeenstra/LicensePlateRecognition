022d71e609a6e905d627dd5431bf635077184917
Zach Veenstra <zach.veen.gr@gmail.com>
2023-11-28 12:06:15 -0500

    Extracted image extraction logic into a function.

--- src/plate_recognition.py
+++ src/plate_recognition.py
@@ -1,10 +1,8 @@
 
-# import easyocr
+import easyocr
 import matplotlib.pyplot as plt
 import numpy as np
 from urllib.request import urlopen
-import subprocess
-import shlex
 import csv
 import cv2
 
@@ -14,25 +12,43 @@ DESCRIPTION_INDEX = 1
 PLATE_IMAGE_LINK_INDEX = 2
 
 
-with open(CSV_PATH) as license_plates_csv:
-    csv_reader = csv.reader(license_plates_csv)
-    for row in csv_reader:
-        # Skip the first line, which only contains column titles.
-        if csv_reader.line_num == 1:
-            continue
+def main():
+    with open(CSV_PATH) as license_plates_csv:
+        csv_reader = csv.reader(license_plates_csv)
+        for row in csv_reader:
+            # Skip the first line, which only contains column titles.
+            if csv_reader.line_num == 1:
+                continue
 
-        image_url = f"{row[PLATE_IMAGE_LINK_INDEX][9:-1]}"
-        image_type = image_url[image_url.rindex('.')+1: ]
-
-        if image_type.casefold() == 'gif':
-            continue
-
-        resp = urlopen(image_url)
-        image = np.asarray(bytearray(resp.read()), dtype="uint8")
-        image = cv2.imdecode(image, cv2.IMREAD_COLOR) # The image object
+            try:
+                image = getImage(row)
+            except Exception:
+                continue
 
 
-        plt.imshow(image)
-        plt.show()
+            plt.imshow(image)
+            plt.show()
 
-        break
+            break
+
+
+def getImage(row):
+    """
+    Extract an image from a row in a csv table.
+    A TypeError exception is raised if a gif is given as an argument
+    """
+    image_url = f"{row[PLATE_IMAGE_LINK_INDEX][9:-1]}"
+    image_type = image_url[image_url.rindex('.')+1: ]
+
+    # We don't support the gif file type for now.
+    if image_type.casefold() == 'gif':
+        raise TypeError
+
+    resp = urlopen(image_url)
+    image = np.asarray(bytearray(resp.read()), dtype="uint8")
+    image = cv2.imdecode(image, cv2.IMREAD_COLOR) # The image object
+
+    return image
+
+if __name__ == "__main__":
+    main()
