5dfb5a61a1c8cb7dd85e12cdaf114cd46a11ae62
Huy <112675341+BestAsian@users.noreply.github.com>
2023-11-29 19:51:12 -0500

    Changed a few things: Updated Indexes, URL to filepath instead, Checks all words read instead of only one .
    Updated Indexes, 
    URL to filepath instead, 
    Checks all words read instead of only one .
    
    Things to Change:
    - As Zach mentioned: we should create equivalences or similar words.
    
    I noticed that sometimes the model registered an extra letter, making it incorrect. Should we let this pass?
    
    The data set has some images that are intentionally in the wrong place that was meant for training a CNN. Ex: Arkansas license plate in Alabama folder. 
    IDK what to do here.
    
    For Guam:
    GUAM USA is read as a single entity. So it is processed as incorrect.
    
    Probably some more missed. Should we ignore?

--- src/plate_recognition.py
+++ src/plate_recognition.py
@@ -4,14 +4,15 @@ import numpy as np
 from urllib.request import urlopen
 import csv
 import cv2
+import re
 
 
-CSV_PATH = './data/license_plates.csv'
-STATE_INDEX = 0
-DESCRIPTION_INDEX = 1
-PLATE_IMAGE_LINK_INDEX = 2
+CSV_PATH = './plates.csv'
+STATE_INDEX = 2
+DESCRIPTION_INDEX = 2
+PLATE_IMAGE_LINK_INDEX = 1
 
-
+    
 def main():
     """
     Go through each license plate in the CSV and read the text from the
@@ -31,44 +32,57 @@ def main():
             except Exception:
                 continue
 
-            state = row[STATE_INDEX]
+            state = row[STATE_INDEX].capitalize()
 
-            character_reader = easyocr.Reader(['en'], gpu=False) # Note: if you have a GPU, set this to True!
+            character_reader = easyocr.Reader(['en'], gpu=True)  # Note: if you have a GPU, set this to True!
 
             text = character_reader.readtext(license_plate)
-            
+            print("Here is the text: ", text)
             # TODO: Read the state in a better way than just getting a certain element from the text.
-            state_read = text[0][1]
-            
+            # state_read = text[0][1] NOT ALWAYS AT THIS INDEX
             # TODO: Create table with state spelling equivilencies. Ex: California - CA, CAL, CALIFORNIA
 
             total_plates += 1
-            if state == state_read:
-                print(f"{state_read} == {state}")
-                num_plates_correctly_identified += 1
-            else:
-                print(f"{state_read} != {state}")
-    
-    print(f"Percentage correct = {num_plates_correctly_identified / total_plates}")
+            for x in range(len(text)):
+                word = text[x][1]
+                if state == word.capitalize():
+                    print(f"{word} == {state}")
+                    num_plates_correctly_identified += 1
+                    break
+                else:
+                    print(f"{word} != {state}")
 
+    if total_plates != 0:
+        print(f"Percentage correct = {num_plates_correctly_identified / total_plates}")
+    else:
+        print("Failed to read")
 
 def getImage(row):
     """
     Extract an image from a row in a csv table.
     A TypeError exception is raised if a gif is given as an argument
     """
-    image_url = f"{row[PLATE_IMAGE_LINK_INDEX][9:-1]}"
-    image_type = image_url[image_url.rindex('.')+1: ]
-
+    image_url = f"{row[PLATE_IMAGE_LINK_INDEX]}"
+    image_type = image_url[(image_url.rindex('.') + 1):]
+    print(image_url)
     # We don't support the gif file type for now.
     if image_type.casefold() == 'gif':
         raise TypeError
-
-    resp = urlopen(image_url)
+    image = cv2.imread(image_url)
+    """
+    # Breaks if we try to display image
+    if image is not None:
+        # Display the image (optional)
+        cv2.imshow('Image', image)
+        cv2.waitKey(1)
+        cv2.destroyAllWindows()
+    
+    resp = urlopen(image_link)
     image = np.asarray(bytearray(resp.read()), dtype="uint8")
-    image = cv2.imdecode(image, cv2.IMREAD_COLOR) # The image object
-
+    image = cv2.imdecode(image, cv2.IMREAD_COLOR)  # The image object
+    """
     return image
+
 
 if __name__ == "__main__":
     main()
