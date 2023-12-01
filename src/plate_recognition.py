6df6ae207f94ceea595f8cb3068afd6316cb0369
Dgenord17 <99490928+Dgenord17@users.noreply.github.com>
2023-11-30 22:49:43 -0500

    Added equivalencies and regex
    Now iterates through all equivalencies for each plate until it finds one that is contained within the words extracted from the plate.  
    Will check to see if the plate it thought it was based on the equivalency is actually from the right state by comparing the indices from the csv and the list of equivalencies.  Number correct will only be incremented if it was actually correct, as just finding an equivalency does not necessarily mean that the state was correct (i.e. Washington D.C. will probably get identified as Washington most times, same with West Virginia and Virginia)

--- src/plate_recognition.py
+++ src/plate_recognition.py
@@ -11,9 +11,69 @@ CSV_PATH = './plates.csv'
 STATE_INDEX = 2
 DESCRIPTION_INDEX = 2
 PLATE_IMAGE_LINK_INDEX = 1
+STATE_ID_INDEX = 0
 
     
 def main():
+    words = [
+        ["Alabama", "Bama"],
+        ["Alaska", "AK"],
+        ["American Samoa", "Samoa"],
+        ["Arizona", "AZ", "Ariz"],
+        ["Arkansas", "Ark"],
+        ["California", "Calif", "Cali", "Cal"],
+        ["CNMI", "Saipan", "Mariana", "Northern Mariana", "Pacific", "NorthernMariana"],
+        ["Colorado", "Colo", "Color"],
+        ["Connecticut", "CT", "Conn"],
+        ["Delaware", "Del"],
+        ["Florida", "FL", "Fla"],
+        ["Georgia", "Peach"],
+        ["Guam"],
+        ["Hawaii", "Aloha"],
+        ["Idaho", "Potato"],
+        ["Illinois", "IL", "Ill"],
+        ["Indiana", "Ind"],
+        ["Iowa"],
+        ["Kansas", "KS", "Kan"],
+        ["Kentucky", "KY"],
+        ["Louisiana"],
+        ["Maine", "Vacationland", "Vacation"],
+        ["Maryland", "MD"],
+        ["Massachusetts", "Mass"],
+        ["Michigan", "Mich", "Pure"],
+        ["Minnesota", "MN", "Minn"],
+        ["Mississippi", "MS"],
+        ["Missouri"],
+        ["Montana", "MT"],
+        ["Nebraska", "Neb"],
+        ["Nevada", "NV", "Nev"],
+        ["New Hampshire", "NH", "N.H.", "Hampshire", "NewHampshire"],
+        ["New Jersey", "NJ", "N.J.", "Jersey", "Garden", "NewJersey"],
+        ["New Mexico", "NM", "N.M.", "Mexico", "Enchantment", "NewMexico"],
+        ["New York", "NY", "N.Y.", "York", "NewYork"],
+        ["North Carolina", "N.C.", "NorthCarolina"],
+        ["North Dakota", "ND", "N.D.", "NorthDakota", "N.Dakota", "NDakota"],
+        ["Ohio"],
+        ["Oklahoma", "OK", "Okla"],
+        ["Oregon", "Ore"],
+        ["Pennsylvania", "PA", "Penn", "Penna"],
+        ["Puerto Rico", "PuertoRico", "Puerto", "Rico"],
+        ["Rhode Island", "R.I.", "RhodeIsland", "Rhode"],
+        ["South Carolina", "SC", "S.C.", "SouthCarolina"],
+        ["South Dakota", "SD", "S.D.", "SouthDakota", "S.Dak", "S.Dakota", "SDak", "SDakota"],
+        ["Tennessee", "TN", "Tenn"],
+        ["Texas", "TX", "Tex"],
+        ["Virgin", "Islands", "VirginIslands"],
+        ["Utah"],
+        ["Vermont", "VT"],
+        ["Virginia"],
+        ["Washington", "WA", "Wash"],
+        ["Columbia", "Washington DC", "Washington D.C.", "WashingtonDC", "DC", "D.C"],
+        ["West Virginia", "WV", "W.Va.", "WestVirginia", "West"],
+        ["Wisconsin", "Wisc"],
+        ["Wyoming", "WY", "Wyo"]
+    ]
+
     """
     Go through each license plate in the CSV and read the text from the
     image, calculating the number of which were correctly identified.
@@ -22,7 +82,7 @@ def main():
     num_plates_correctly_identified = 0
     with open(CSV_PATH) as license_plates_csv:
         csv_reader = csv.reader(license_plates_csv)
-        for row in csv_reader:
+        for n, row in enumerate(csv_reader):
             # Skip the first line, which only contains column titles.
             if csv_reader.line_num == 1:
                 continue
@@ -32,7 +92,7 @@ def main():
             except Exception:
                 continue
 
-            state = row[STATE_INDEX].capitalize()
+            stateId = row[STATE_ID_INDEX]
 
             character_reader = easyocr.Reader(['en'], gpu=True)  # Note: if you have a GPU, set this to True!
 
@@ -40,17 +100,32 @@ def main():
             print("Here is the text: ", text)
             # TODO: Read the state in a better way than just getting a certain element from the text.
             # state_read = text[0][1] NOT ALWAYS AT THIS INDEX
-            # TODO: Create table with state spelling equivilencies. Ex: California - CA, CAL, CALIFORNIA
+            # TODO: Create table with state spelling equivalencies. Ex: California - CA, CAL, CALIFORNIA
 
             total_plates += 1
+            isLooping = True
             for x in range(len(text)):
                 word = text[x][1]
-                if state == word.capitalize():
+                for i, sublist in enumerate(words):
+                    for equivalence in sublist:
+                        if equivalence.lower() in word.lower():
+                            print(f"{word} contained {equivalence}")
+                            if stateId == i:
+                                num_plates_correctly_identified += 1
+                            isLooping = False
+                            break
+                        else:
+                            print(f"{word} didn't contain {equivalence}")
+                    if not isLooping:
+                        break
+                if not isLooping:
+                    break
+                """if state == word.capitalize():
                     print(f"{word} == {state}")
                     num_plates_correctly_identified += 1
                     break
                 else:
-                    print(f"{word} != {state}")
+                    print(f"{word} != {state}")"""
 
     if total_plates != 0:
         print(f"Percentage correct = {num_plates_correctly_identified / total_plates}")
